fb70fa2e0ad8cbb651d0d43cd3fc2758ccbfef12
lampajr <lampa9559@gmail.com>
2019-04-17 15:43:01 +0200

    online streamer on_status method implemented

--- ptdc/streamer.py
+++ ptdc/streamer.py
@@ -1,13 +1,15 @@
 import tweepy
-import json
-from abc import ABC, abstractmethod
+import logging
+
 from ptdc import utils
+from ptdc.data import DataCollector
 
 
-class Streamer(ABC, tweepy.StreamListener):
+class Streamer(tweepy.StreamListener):
 
-    def __init__(self, api,  time_limit=None, path=None):
-        """ Streamer constructor
+    def __init__(self, api, collector=None,  time_limit=None, path="../data/default_stream_file.json"):
+
+        """ Streamer constructor, it represents an offline streamer, store streaming data into a file
          :param api: tweepy api
          :param time_limit: duration of the streaming, if None last forever
          :param path: file's location where saving the data collected, if None print on the std output """
@@ -18,39 +20,47 @@ class Streamer(ABC, tweepy.StreamListener):
         self.path = path
         self.start_time = 0
         self.file = None
+        self.collector = DataCollector(api=self.api) if collector is None else collector
 
     def on_connect(self):
 
         """ called when the connection with
-                the streaming server is established """
+        the streaming server is established """
 
         self.start_time = utils.get_time()
-        self.on_start()
+        logging.debug("Streaming started at {}".format(utils.get_date()))
 
-    def on_error(self, status_code):
-        print(status_code)
+        if self.path is not None:
+            # open or create a new file
+            try:
+                self.file = open(self.path, "a")
+            except FileNotFoundError:
+                self.file = open(self.path, "w")
 
     def on_data(self, raw_data):
 
-        """ called when raw data is received from stream """
+        """ Called when new data is available """
 
+        # if enough time is passed stop streaming
         if self.time_limit is not None and (utils.get_time() - self.start_time) > self.time_limit:
-            # stop streaming
+            # if file has been opened, close it
             if self.file is not None:
                 self.file.close()
                 self.file = None
+            # stop connection to he streaming server
+            logging.debug("Streaming terminated at {}".format(utils.get_date()))
+            logging.debug("Streaming duration = {} seconds".format(self.time_limit))
             return False
-        else:
-            self.process_data(raw_data)
-            return True
+        elif self.file is not None:
+            # print the raw data on the file
+            self.file.write(raw_data)
+            self.file.write("\n")
 
-    @abstractmethod
-    def process_data(self, raw_data):
-        pass
+        # call on_data of the superclass
+        super(Streamer, self).on_data(raw_data=raw_data)
 
-    @abstractmethod
-    def on_start(self):
-        pass
+    def on_error(self, status_code):
+        print(status_code)
 
     def stream(self, follow=None, track=None, is_async=False, locations=None,
                stall_warnings=False, languages=None, encoding='utf8', filter_level=None):
@@ -62,46 +72,19 @@ class Streamer(ABC, tweepy.StreamListener):
                        stall_warnings=stall_warnings, languages=languages, encoding=encoding, filter_level=filter_level)
 
 
-class OfflineStreamer(Streamer):
-
-    def __init__(self, api, path, time_limit=None):
-
-        """ Offline streamer, requires a path where saving the streaming data """
-
-        super(OfflineStreamer, self).__init__(api, time_limit=time_limit, path=path)
-
-    def process_data(self, raw_data):
-        # print the raw data on the file
-        self.file.write(raw_data)
-        self.file.write("\n")
-
-    def on_start(self):
-        try:
-            self.file = open(self.path, "a")
-        except FileNotFoundError:
-            self.file = open(self.path, "w")
-
-    def load_data_from_file(self, json_path):
-
-        """ load the user's data starting from streaming data in json format
-        :param json_path: path of the file where streaming data is saved"""
-
-        pass
-
-
 class OnlineStreamer(Streamer):
 
-    def __init__(self, api, time_limit=None):
+    """ Subclass of Streamer that collects data during the streaming, generating the corresponding DataFrame/s"""
+
+    def __init__(self, api, time_limit=None, path="../data/default_stream_file.json"):
 
         """ Online streamer, collects data online during streaming
         w/o saving it into a specific text file """
 
-        super(OnlineStreamer, self).__init__(api=api, time_limit=time_limit, path=None)
+        super(OnlineStreamer, self).__init__(api=api, time_limit=time_limit, path=path)
 
-    def process_data(self, raw_data):
-        # TODO: implement online
-        pass
+    def on_status(self, status):
 
-    def on_start(self):
-        # do nothing
-        pass
+        """ called when raw data is received from stream """
+
+        self.collector.collect_user(screen_name=status.user.screen_name)
