da75de915e111e3bcbc6c75f49f75ebcfec6ef47
lampajr <lampa9559@gmail.com>
2019-04-17 11:39:31 +0200

    added tweets collector method

--- ptdc/data.py
+++ ptdc/data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import tweepy
 
 
 _all_user_attributes = np.array(["id", "name", "screen_name", "location", "url", "description", "protected",
@@ -13,9 +14,13 @@ _all_user_attributes = np.array(["id", "name", "screen_name", "location", "url",
 
 _new_user_attributes = np.array(["profile_crawled", "is_suspended"])
 
-_all_tweets_attributes = np.array([])
+_all_tweets_attributes = np.array(["id", "created_at", "full_text", "lang", "retweeted", "coordinates", "retweet_count",
+                                   "favorite_count", "source", "place", "geo", "truncated"])
 
-_new_tweets_attributes = np.array([])
+_new_tweets_attributes = np.array(["user_id"])
+
+
+# attributes' sizes
 
 _user_attr_size = len(_all_user_attributes) + len(_new_user_attributes)
 _tweet_attr_size = len(_all_tweets_attributes) + len(_new_tweets_attributes)
@@ -92,10 +97,13 @@ User object structure
 
 class DataCollector(object):
 
-    """ Data Collector that stores the accounts dataset:
+    """ Data Collector that stores the DataFrames of the account and/or their tweets:
     provides methods for handling the data, adding new users, removing users.."""
 
-    def __init__(self, api, user_attrs_selectors=None, tweets_attrs_selectors=None):
+    def __init__(self, api, user_attrs_selectors=None, collect_users=True,
+                 tweets_attrs_selectors=None, collect_tweets=True):
+        self._collect_tweets = collect_tweets
+        self._collect_users = collect_users
         self._user_attrs_selectors = np.ones(_user_attr_size, dtype=bool) if user_attrs_selectors is None else \
             user_attrs_selectors
         self._tweets_attrs_selectors = np.ones(_tweet_attr_size, dtype=bool) if tweets_attrs_selectors is None else \
@@ -108,13 +116,23 @@ class DataCollector(object):
         # Twitter api for making query
         self._api = api
 
-    def get_data(self):
+    def get_users_dataset(self):
 
-        """ return the pandas DataFrame"""
+        """ Return pandas DataFrame containing collected accounts"""
 
         return self._user_dataset
 
-    def add_user(self, screen_name):
+    def get_tweets_dataset(self):
+
+        """ Returns pandas DataFrame containing collected tweets"""
+
+        return self._tweets_dataset
+
+    def collect_user(self, screen_name):
+
+        """ Collect all the information about a specific Account
+        :param screen_name: the screen_name/id of the account"""
+
         user = self._api.get_user(screen_name)
 
         # TODO: collect timeline of user
@@ -122,10 +140,50 @@ class DataCollector(object):
         # retrieve all information about single user
         raw_data = pd.Series([getattr(user, attr) for attr in _all_user_attributes], index=self._user_dataset.columns)
 
-        # TODO: add new attributes data, manually
+
         raw_data = raw_data[self._user_attrs_selectors]
         self._user_dataset = self._user_dataset.append(raw_data, ignore_index=True)
 
+    def _process_user(self, user):
 
-    def to_csv(self, filename):
+        """ Process a single user, collecting all the information """
+
+        raw_data = [getattr(user, attr) for attr in _all_user_attributes]
+        # TODO: add new attributes data, manually
+        tmp_tweets = self.collect_tweets(screen_name=user.screen_name)
+
+        raw_data = pd.Series(raw_data, index=self._user_dataset.columns)
+        return raw_data
+
+    def collect_tweets(self, screen_name, n_tweets=20):
+
+        """ Collect some tweets for a specific account, retrieving their attributes
+        :param screen_name: screen_name of the account for which retrieve their tweets
+        :param n_tweets: number of tweets to collect for that account
+        :return: DataFrame containing all tweets collected for this user"""
+
+        temporary_tweets_set = create_twitter_tweets_dataframe(self._tweets_attrs_selectors)
+        for tweet in tweepy.Cursor(self._api.user_timeline, id=screen_name,  tweet_mode='extended').items(n_tweets):
+            temporary_tweets_set = temporary_tweets_set.append(self._process_tweet(tweet=tweet), ignore_index=True)
+
+        return temporary_tweets_set
+
+    def _process_tweet(self, tweet):
+
+        """ Process a single tweet
+        :param tweet: tweet that has to be processed, by extracting all information
+        :return: pandas Series containing all the information for that tweet"""
+
+        raw_data = [getattr(tweet, attr) for attr in _all_tweets_attributes]
+        raw_data.append(tweet.user.id)
+
+        raw_data = pd.Series(raw_data, index=self._tweets_dataset.columns)
+
+        raw_data = raw_data[self._tweets_attrs_selectors]
+
+        if self._collect_tweets:
+            self._tweets_dataset = self._tweets_dataset.append(raw_data, ignore_index=True)
+        return raw_data
+
+    def save_to_csv(self, filename):
         self._user_dataset.to_csv(path_or_buf=filename, index=False)
