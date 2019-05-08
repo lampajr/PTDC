b3b6009011c229974eec3ce72406fc893ae029fb
lampajr <lampa9559@gmail.com>
2019-05-08 14:30:19 +0200

    Removed data file Moved default features in collector module

--- ptdc/collector.py
+++ ptdc/collector.py
@@ -10,12 +10,83 @@ the data into Pandas DataFrame.
 
 import logging
 from abc import ABC, abstractmethod
+from functools import reduce
 
 import numpy as np
 import pandas as pd
 import tweepy
 
-from ptdc import data
+from ptdc.support import get_attribute, get_retweeted_user_id, get_retweeted_status, get_quoted_user_id, get_media, \
+    get_date
+
+default_account_features = {"id": get_attribute,
+                            "name": get_attribute,
+                            "screen_name": get_attribute,
+                            "location": get_attribute,
+                            "url": get_attribute,
+                            "description": get_attribute,
+                            "protected": get_attribute,
+                            "verified": get_attribute,
+                            "followers_count": get_attribute,
+                            "friends_count": get_attribute,
+                            "listed_count": get_attribute,
+                            "favourites_count": get_attribute,
+                            "statuses_count": get_attribute,
+                            "created_at": get_attribute,
+                            "utc_offset": get_attribute,
+                            "time_zone": get_attribute,
+                            "geo_enabled": get_attribute,
+                            "lang": get_attribute,
+                            "contributors_enabled": get_attribute,
+                            "profile_background_color": get_attribute,
+                            "profile_background_image_url": get_attribute,
+                            "profile_background_image_url_https": get_attribute,
+                            "profile_background_tile": get_attribute,
+                            "profile_image_url": get_attribute,
+                            "profile_image_url_https": get_attribute,
+                            "profile_link_color": get_attribute,
+                            "profile_text_color": get_attribute,
+                            "profile_use_background_image": get_attribute,
+                            "default_profile": get_attribute,
+                            "default_profile_image": get_attribute,
+                            "profile_crawled": lambda x, y: get_date(),
+                            "is_suspended": lambda x, y: 0,
+                            "following_followers_ratio": lambda user, _: user.friends_count / user.followers_count if user.followers_count != 0 else None,
+                            "followers_following_ratio": lambda user, _: user.followers_count / user.friends_count if user.friends_count != 0 else None}
+
+
+default_account_timeline_features = {"n_statuses_collected": lambda statuses_data, _: statuses_data.shape[0],
+                                     "mean_status_length": lambda statuses_data, _: statuses_data["text_length"].mean(),
+                                     "media_shared_urls": lambda statuses_data, _: reduce(lambda x,y: x+y, [], [x for x in statuses_data["media_urls"] if x is not None]),
+                                     "mean_shared_media": lambda statuses_data, _: (len(reduce(lambda x,y: x+y, [], [x for x in statuses_data["media_urls"] if x is not None])) / statuses_data.shape[0]) if statuses_data.shape[0] != 0 else None,
+                                     "quoted_user_ids": lambda statuses_data, _: [x for x in statuses_data["quoted_user_id"] if x is not None],
+                                     "replied_status_ids": lambda statuses_data, _: [x for x in statuses_data["in_reply_to_status_id"] if x is not None],
+                                     "replied_user_ids": lambda statuses_data, _: [x for x in statuses_data["in_reply_to_user_id"] if x is not None],
+                                     "retweeted_status_ids": lambda statuses_data, _: [x for x in statuses_data["retweeted_status"] if x is not None],
+                                     "retweeted_user_ids": lambda statuses_data, _: [x for x in statuses_data["retweeted_user_id"] if x is not None]}
+
+
+default_statuses_features = {"id": get_attribute,
+                             "created_at": get_attribute,
+                             "full_text": get_attribute,
+                             "lang": get_attribute,
+                             "coordinates": get_attribute,
+                             "retweet_count": get_attribute,
+                             "favorite_count": get_attribute,
+                             "source": get_attribute,
+                             "place": get_attribute,
+                             "truncated": get_attribute,
+                             "is_quote_status": get_attribute,
+                             "in_reply_to_status_id": get_attribute,
+                             "in_reply_to_user_id": get_attribute,
+                             "in_reply_to_screen_name": get_attribute,
+                             "user_id": lambda status, _: status.user.id,
+                             "text_length": lambda status, _: len(status.full_text),
+                             "hashtags": lambda status, _: [ht["text"] for ht in status.entities["hashtags"]],
+                             "media_urls": lambda status, _: get_media(status),
+                             "quoted_user_id": lambda status, _: get_quoted_user_id(status),
+                             "retweeted_status": lambda status, _: get_retweeted_status(status),
+                             "retweeted_user_id": lambda status, _: get_retweeted_user_id(status)}
 
 
 class Collector(ABC):
@@ -116,8 +187,8 @@ class AccountCollector(Collector):
 
         super(AccountCollector, self).__init__(api=api, debug=debug)
 
-        self._features = data.default_account_features if features is None else features
-        self._timeline_features = data.default_account_timeline_features if timeline_features is None else timeline_features
+        self._features = default_account_features if features is None else features
+        self._timeline_features = default_account_timeline_features if timeline_features is None else timeline_features
 
         self._all_features = np.array(np.concatenate((np.array(list(self._features.keys())), np.array(list(self._timeline_features.keys())))))
 
@@ -211,7 +282,7 @@ class StatusCollector(Collector):
                  debug=True):
         super(StatusCollector, self).__init__(api=api, debug=debug)
 
-        self._features = data.default_statuses_features if features is None else features
+        self._features = default_statuses_features if features is None else features
         self._all_features = np.array(list(self._features.keys()))
 
         self.init_dataset(features=self._all_features)
@@ -290,3 +361,4 @@ class StatusCollector(Collector):
         """
 
         return pd.Series([func(status, attr_name) for attr_name, func in self._features.items()], index=self.dataset().columns)
+
