34c7884a417bd35bce8a8470ed7f337ab4e674f8
lampajr <lampa9559@gmail.com>
2019-05-09 23:14:57 +0200

    Removed count decrement

--- ptdc/streamer.py
+++ ptdc/streamer.py
@@ -202,7 +202,6 @@ class OnlineStreamer(tweepy.StreamListener):
             except (socket.timeout, exceptions.ReadTimeoutError, exceptions.ProtocolError, tweepy.TweepError) as e:
                 logging.warning(e)
                 logging.warning("Reconnecting...")
-                self.count -= 1
                 if self.attempts is not None:
                     self.attempts -= 1
                 continue
